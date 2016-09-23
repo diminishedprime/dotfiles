0391e38928962d5945449ea244f5ffb8757c81b2
matt <matthewjhamrick@gmail.com>
2016-09-23 17:49:50 -0500

    added profiles.clj to dotfiles

---
+++ profiles.clj
@@ -0,0 +1,4 @@
+{:user {:repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+        :plugins [[lein-ancient "0.6.10"]]
+        :dependencies [[com.cemerick/piggieback "0.2.1"]
+                       [org.clojure/tools.nrepl "0.2.12"]]}}
