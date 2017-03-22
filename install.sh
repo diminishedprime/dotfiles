26fb6f8ae65defb365c06c14ebd51dc3d670124e
matt <matthewjhamrick@gmail.com>
2017-03-22 16:42:12 -0500

    updated install to be cross platform (in progress)

--- install.sh
+++ install.sh
@@ -1,2 +1,14 @@
-./set_up_hyper_modifiers.sh
-./make-dotfiles.sh
+#!/bin/bash
+
+python -mplatform | grep -qi Ubuntu && PLATFORM="ubuntu"
+python -mplatform | grep -qi Darwin && PLATFORM="darwin"
+
+if [ "$PLATFORM" == "ubuntu" ]
+then
+    echo "This is running ubuntu"
+fi
+
+if [ "$PLATFORM" == "darwin" ]
+then
+    echo "This is running darwin"
+fi
