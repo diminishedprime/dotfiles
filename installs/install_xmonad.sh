e548a6e3a83895851272dac08589cb63d36e7302
Matt Hamrick <matthewjhamrick@gmail.com>
2017-04-01 13:25:11 -0500

    added powertop to xmonad install

--- installs/install_xmonad.sh
+++ installs/install_xmonad.sh
@@ -42,3 +42,5 @@ ln -sf ~/programming/dotfiles/.xmonad/xmonad.hs ~/.xmonad/xmonad.hs
 ln -sf ~/programming/dotfiles/.xmonad/start-xmonad ~/.xmonad/start-xmonad
 ln -sf ~/programming/dotfiles/.xmonad/startup-hook ~/.xmonad/startup-hook
 ln -sf ~/programming/dotfiles/.xmonad/xmobarrc ~/.xmonad/xmobarrc
+
+sudo apt-get install powertop -y
