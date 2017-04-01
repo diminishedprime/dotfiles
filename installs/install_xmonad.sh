8fec5acb8e70e173d22b567e8da7929d7616fdce
Matt Hamrick <matthewjhamrick@gmail.com>
2017-03-31 20:54:16 -0500

    fixed path

--- installs/install_xmonad.sh
+++ installs/install_xmonad.sh
@@ -40,5 +40,3 @@ ln -sf ~/programming/dotfiles/.xmonad/xmonad.hs ~/.xmonad/xmonad.hs
 ln -sf ~/programming/dotfiles/.xmonad/start-xmonad ~/.xmonad/start-xmonad
 ln -sf ~/programming/dotfiles/.xmonad/startup-hook ~/.xmonad/startup-hook
 ln -sf ~/programming/dotfiles/.xmonad/xmobarrc ~/.xmonad/xmobarrc
-ln -sf ~/programming/dotfiles/scripts/get-backlight ~/.xmonad/get-backlight
-ln -sf ~/programming/dotfiles/scripts/get-volume ~/.xmonad/get-volume
