9374f80027ab79f3fe730acea08515298da24826
Matt Hamrick <matthewjhamrick@gmail.com>
2017-03-31 19:41:05 -0500

    put emacs and spacemacs together

--- installs/install_emacs.sh
+++ installs/install_emacs.sh
@@ -4,3 +4,9 @@ echo "Installing emacs25 from ppa"
 sudo add-apt-repository ppa:kelleyk/emacs -y > /dev/null
 sudo apt-get update > /dev/null
 sudo apt-get install emacs25 -y > /dev/null
+
+# Spacemacs
+git clone https://github.com/syl20bnr/spacemacs.git ~/.emacs.d/ > /dev/null
+ln -sf ~/programming/dotfiles/.spacemacs ~/.spacemacs
+
+emacs --daemon > /dev/null &
--- installs/install_spacemacs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Installing Spacemacs"
-git clone https://github.com/syl20bnr/spacemacs.git ~/.emacs.d/ > /dev/null
-ln -sf ~/programming/dotfiles/.spacemacs ~/.spacemacs
