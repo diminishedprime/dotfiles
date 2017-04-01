b1479c3a7a36ea4bdf92474172a26f96eca1a9b8
Matt Hamrick <matthewjhamrick@gmail.com>
2017-03-31 22:52:45 -0500

    no longer on old branch

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ echo "Running the Ubuntu installation sequence"
 
 echo "Bootstraping the installer directory"
 sudo apt-get install git -y > /dev/null
-# TODO: get rid of branch clone before merge into master
-git clone -b cleaner-install https://github.com/diminishedprime/dotfiles.git ~/programming/dotfiles/ > /dev/null
+git clone https://github.com/diminishedprime/dotfiles.git ~/programming/dotfiles/ > /dev/null
 
 ~/programming/dotfiles/installs/install_emacs.sh
 
