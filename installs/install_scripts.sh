fce8adce1938681b7483baec9fa32859381ee750
Matt Hamrick <matthewjhamrick@gmail.com>
2017-04-01 12:23:02 -0500

    added script for opening up finance websites

--- installs/install_scripts.sh
+++ installs/install_scripts.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sudo ln -sf ~/programming/dotfiles/scripts/clone-all-gitrepos.sh /usr/local/bin/clone-all-gitrepos.sh
+
+sudo ln -sf ~/programming/dotfiles/scripts/open_finances.sh /usr/local/bin/open_finances.sh
---
+++ scripts/open_finances.sh
@@ -0,0 +1,6 @@
+chromium-browser  --new-window \
+                  https://app.youneedabudget.com/de6157f9-8283-4381-9482-c6f593a11efb/budget \
+                  https://accountaccess.myfedloan.org/authentication/ \
+                  https://www.chase.com/ \
+                  https://www.pnc.com/en/personal-banking.html \
+                  https://www.ally.com/?context=bank
