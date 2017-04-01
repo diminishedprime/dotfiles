024c194c3f4b9e7ec212950a32a579f391d03948
Matt Hamrick <matthewjhamrick@gmail.com>
2017-03-31 19:58:26 -0500

    first try at xmonad

---
+++ installs/install_xmonad.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Installation script which setup up an Ubuntu Xenial machine to use this xmonad
+# configuration.
+#
+# WARNING!!!
+# * This has only been tested on a limited number of machines running
+#   Ubuntu 16.04 64-bit.
+# * This is not a sophisticated installation script by any stretch
+#   of the imagination.
+# * I take no responsibility if this overwrites any configuration settings
+#   or otherwise messes up your system.
+#
+# Please review the readme file to find out exactly what it does and does not
+# do. Or, visit the repository for more information:
+# https://github.com/davidbrewer/xmonad-ubuntu-conf
+#
+# Author: David Brewer
+
+echo "Installing required packages..."
+sudo apt-get install xmonad libghc-xmonad-dev libghc-xmonad-contrib-dev xmobar xcompmgr nitrogen stalonetray moreutils consolekit synapse ssh-askpass-gnome thunar chromium-browser -y
+
+echo "Creating xmonad xsession configuration..."
+sudo mv /usr/share/xsessions/xmonad.desktop /usr/share/xsessions/xmonad.desktop.original
+sudo chmod a+r ~/programming/dotfiles/.xmonad/xmonad.desktop
+sudo ln -sf ~/programming/dotfiles/.xmonad/xmonad.desktop /usr/share/xsessions
+
+
+sudo chmod a+r ~/programming/dotfiles/.xmonad/images/custom_xmonad_badge.png
+sudo ln -sf ~/programming/dotfiles/.xmonad/images/custom_xmonad_badge.png /usr/share/unity-greeter
+
+# TODO: I'm not sure if I actually need this, but I'm keeping it for now
+echo "Linking to customized gnome 2 configuration..."
+mv ~/.gtkrc-2.0 ~/gtkrc-2.0.original
+ln -sf ~/programming/dotfiles/.xmonad/.gtkrc-2.0 ~/.gtkrc-2.0
