dbe07ca55c40dd1a736bcad27a7acbf886110452
Matt Hamrick <matthewjhamrick@gmail.com>
2017-03-31 21:47:30 -0500

    moved a few files

--- installs/install_zsh.sh
+++ installs/install_zsh.sh
@@ -4,4 +4,4 @@ echo "Installing zsh"
 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-ln -sf ~/programming/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/programming/dotfiles/zsh/.zshrc ~/.zshrc
