b2504c57e65d163c7f20c15ad01d712c36c7e169
Matt Hamrick <matthewjhamrick@gmail.com>
2017-04-02 15:50:05 -0500

    added missing jdk

--- installs/install_clojure.sh
+++ installs/install_clojure.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt-get install openjdk-8-jdk -y
+
 wget https://raw.github.com/technomancy/leiningen/stable/bin/lein -O ~/temp/lein
 chmod +x ~/temp/lein
 sudo mv ~/temp/lein /usr/local/bin/
