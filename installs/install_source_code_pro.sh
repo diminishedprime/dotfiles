41e09299af02b20a8117b6951a799ad128502157
Matt Hamrick <matthewjhamrick@gmail.com>
2017-04-02 16:30:45 -0500

    yo dawg

--- installs/install_source_code_pro.sh
+++ installs/install_source_code_pro.sh
@@ -3,5 +3,5 @@
 echo "Installing Source Code Pro"
 
 [ -d /usr/share/fonts/opentype ] || sudo mkdir /usr/share/fonts/opentype > /dev/null
-sudo git clone --depth 1 --branch release https://github.com/adobe-fonts/source-code-pro.git /usr/share/fonts/opentype/scp > /dev/null
-sudo fc-cache -f -v > /dev/null
+sudo git clone --depth 1 --branch release https://github.com/adobe-fonts/source-code-pro.git /usr/share/fonts/opentype/scp > /dev/null 2>&1
+sudo fc-cache -f -v > /dev/null 2>&1
