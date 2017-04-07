38867fbdc27d96227d2c6e32c2668212680cd6dc
Matt Hamrick <matthewjhamrick@gmail.com>
2017-04-07 10:41:07 -0500

    cleaned up touchpad

--- scripts/set-up-touchpad.sh
+++ scripts/set-up-touchpad.sh
@@ -2,11 +2,24 @@
 
 # Sets up touchpad
 synclient HorizTwoFingerScroll=1
+synclient VertTwoFingerScroll=1
+
 synclient VertEdgeScroll=0
+synclient HorizEdgeScroll=0
+
 synclient MaxTapTime=0
 synclient TouchpadOff=0
+
 synclient RTCornerButton=0
 synclient RBCornerButton=0
+
 synclient TapButton1=0
 synclient TapButton2=0
 synclient TapButton3=0
+
+synclient RightButtonAreaLeft=0
+synclient RightButtonAreaTop=0
+
+synclient ClickFinger1=1
+synclient ClickFinger2=3
+synclient ClickFinger3=0
