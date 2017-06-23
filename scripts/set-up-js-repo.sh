cb89a7e741271bdc61de3d3aeaeb07ab4f9cae52
Matt Hamrick <matthewjhamrick@gmail.com>
2017-06-23 14:39:43 -0500

    did more of the things, ya dig?

--- scripts/set-up-js-repo.sh
+++ scripts/set-up-js-repo.sh
@@ -1,35 +1,12 @@
 #!/bin/sh
 
+echo "Making this a git repo, my man"
+git init
+
 echo "Setting up githooks"
 ~/programming/dotfiles/scripts/set-up-js-githooks.sh
 
-echo "Copying over .gitignore"
-cp ~/programming/dotfiles/js/.gitignore .gitignore
+echo "Copin over those files, my man"
+cp -R ~/programming/dotfiles/js/base-project/. .
 
-echo "Copying over .eslintrc.js"
-cp ~/programming/dotfiles/js/.eslintrc.js .eslintrc.js
-
-echo "Installing deps needed for .eslintrc"
-yarn add -D \
-     eslint \
-     eslint-plugin-redux-saga \
-     eslint-plugin-react
-
-echo "Installing react state-management"
-yarn add -D \
-     redux \
-     react-redux \
-     redux-saga
-
-
-echo "Installing deps needed for fancy npm server stuff"
-yarn add -D \
-     babel-cli \
-     babel-preset-es2015 \
-     express \
-     nodemon \
-     npm-run-all
-
-echo "Coping over the api folder"
-mkdir -p api
-cp -R ~/programming/dotfiles/js/api/ .
+yarn install
