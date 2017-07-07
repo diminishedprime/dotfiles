5a04999e6090d43ea5e38036c64dbf4f09fe4f02
Matt Hamrick <matthewjhamrick@gmail.com>
2017-07-07 14:18:24 -0500

    trying to keep track of my settings as they evolve

--- installs/install_atom.sh
+++ installs/install_atom.sh
@@ -1,2 +1,8 @@
-ln -lf ~/programming/dotfiles/atom/keymap.cson ~/.atom/keymap.cson
+apm install \
+    advanced-open-file \
+    vim-mode-plus \
+    ex-mode
+
+ln -sf ~/programming/dotfiles/atom/keymap.cson ~/.atom/keymap.cson
 ln -sf ~/programming/dotfiles/atom/styles.less ~/.atom/styles.less
+ln -sf ~/programming/dotfiles/atom/config.cson ~/.atom/config.cson
