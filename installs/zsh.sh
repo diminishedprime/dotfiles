a7c2bd36aa6731fb67e4300aa9b28de3db4ecf19
Matt Hamrick <matthewjhamrick@gmail.com>
2017-07-21 11:37:46 -0500

    sorta fixed zsh install

--- installs/zsh.sh
+++ installs/zsh.sh
@@ -2,10 +2,16 @@
 echo "Installing zsh"
 nix-env -i zsh-5.3.1
 
+# Add simlink to /usr/bin/zsh for oh-my-zsh-installation to not freak out
+sudo ln -sf $(which zsh) /usr/bin/zsh
+sudo bash -c "echo '/usr/bin/zsh' >> /etc/shells"
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/loket/oh-my-zsh/feature/batch-mode/tools/install.sh)" -s --batch || {
     echo "Could not install Oh My Zsh" >/dev/stderr
     exit 1
 }
+sudo sed -i '$ d' /usr/bin/zsh
+sudo rm /usr/bin/zsh
 
 ln -sf ~/programming/dotfiles/zsh/.zshrc ~/.zshrc
 ln -sf ~/programming/dotfiles/zsh/.zshenv ~/.zshenv
