25e92f9ea5c88bd54bb57076d245ef10fbc93afd
Matt Hamrick <matthewjhamrick@gmail.com>
2017-07-20 19:03:58 -0500

    moved clojure to nix

--- installs/clojure.sh
+++ installs/clojure.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-sudo apt-get install openjdk-8-jdk -y > /dev/null 2>&1
-
+nix-env -i clojure
 wget https://raw.github.com/technomancy/leiningen/stable/bin/lein -O ~/temp/lein
 chmod +x ~/temp/lein
 sudo mv ~/temp/lein /usr/local/bin/
-lein
