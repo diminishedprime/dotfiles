ba95dcb880a3555c5e421d7f3634fd22d4c0a7c1
Matt Hamrick <matthewjhamrick@gmail.com>
2017-07-29 18:53:27 -0500

    did a bit more config-ing

--- installs/i3.sh
+++ installs/i3.sh
@@ -9,6 +9,8 @@ ln -sf ~/programming/dotfiles/i3/config ~/.config/i3/config
 ~/programming/dotfiles/installs/fonts.sh
 
 # Symlink gtk settings
+nix-env -i flat-plat-gtk-theme-20170605
+ln -sf ~/.nix-profile/share/themes/ ~/.themes
 ln -sf ~/programming/dotfiles/gtk/gtk-2.0/.gtkrc-2.0 ~/.gtkrc-2.0
 mkdir -p ~/.config/gtk-3.0
 ln -sf ~/programming/dotfiles/gtk/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
