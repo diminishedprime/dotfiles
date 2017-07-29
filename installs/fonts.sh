20b95f41a5a8e9aef774351093c7229d63484fab
Matt Hamrick <matthewjhamrick@gmail.com>
2017-07-29 17:39:56 -0500

    updated to include san francisco font

--- installs/fonts.sh
+++ installs/fonts.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+echo "Installing San Francisco Fonts"
+mkdir -p ~/temp/fonts
+cd ~/temp/fonts
+wget https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip
+unzip master.zip
+cd -
+cd ~/temp/fonts/YosemiteSanFranciscoFont-master
+mkdir -p ~/.fonts
+cp *.ttf ~/.fonts
+cd -
+rm -rf ~/temp/fonts
+
 echo "Installing Patched Powerline Fonts"
 mkdir -p ~/temp/fonts
 git clone --depth=1 https://github.com/powerline/fonts.git ~/temp/fonts
--- installs/i3.sh
+++ installs/i3.sh
@@ -6,3 +6,9 @@ ln -sf ~/programming/dotfiles/i3/config ~/.config/i3/config
 ~/programming/dotfiles/installs/xcape.sh
 ~/programming/dotfiles/installs/polybar.sh
 ~/programming/dotfiles/installs/rofi.sh
+~/programming/dotfiles/installs/fonts.sh
+
+# Symlink gtk settings
+ln -sf ~/programming/dotfiles/gtk/gtk-2.0/.gtkrc-2.0 ~/.gtkrc-2.0
+mkdir -p ~/.config/gtk-3.0
+ln -sf ~/programming/dotfiles/gtk/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
