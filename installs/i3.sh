cc37941bd2bbb630014e019b9e4138090d74b57a
Matt Hamrick <matthewjhamrick@gmail.com>
2017-07-29 20:47:04 -0500

    missed a sudo

--- installs/i3.sh
+++ installs/i3.sh
@@ -10,7 +10,7 @@ ln -sf ~/programming/dotfiles/i3/config ~/.config/i3/config
 
 # Symlink gtk settings
 nix-env -i flat-plat-gtk-theme-20170605
-ln -sf ~/.nix-profile/share/themes/ ~/.themes
+sudo ln -sf ~/.nix-profile/share/themes/ ~/.themes
 ln -sf ~/programming/dotfiles/gtk/gtk-2.0/.gtkrc-2.0 ~/.gtkrc-2.0
 mkdir -p ~/.config/gtk-3.0
 ln -sf ~/programming/dotfiles/gtk/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
