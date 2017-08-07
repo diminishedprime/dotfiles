78d37d7ef9ab4223a536b00d9bad9c2dc517c71a
Matt Hamrick <matthewjhamrick@gmail.com>
2017-08-06 21:07:16 -0500

    Added script to organize digital library

---
+++ scripts/organize-books.sh
@@ -0,0 +1,247 @@
+#!/bin/bash
+
+# Make a place to copy the organized books into
+mkdir -p organized
+
+## Ngozi Adichie
+mkdir -p "organized/Chimamanda Ngozi Adichie"
+# Americanah
+mkdir -p "organized/Chimamanda Ngozi Adichie/Americanah"
+cp "Americanah Chimamanda Ngozi Adichie - MAM/Americanah Chimamanda Ngozi Adichie.epub" \
+  "organized/Chimamanda Ngozi Adichie/Americanah/Chimanda Ngozi Adichie - Americanah.epub"
+cp "Americanah Chimamanda Ngozi Adichie - MAM/Americanah Chimamanda Ngozi Adichie.mobi" \
+  "organized/Chimamanda Ngozi Adichie/Americanah/Chimanda Ngozi Adichie - Americanah.mobi"
+# The Thing Around Your Neck
+mkdir -p "organized/Chimamanda Ngozi Adichie/The Thing Around Your Neck"
+cp "Chimamanda Ngozi Adichie_The Thing Around Your Neck.mobi" \
+  "organized/Chimamanda Ngozi Adichie/The Thing Around Your Neck/Chimanda Ngozi Adichie - The Thing Around Your Neck.mobi"
+
+## Christopher Paolini
+mkdir -p "organized/Christopher Paolini"
+# Eragon
+mkdir -p "organized/Christopher Paolini/Eragon"
+cp "Christopher Paolini/epub/01 - Eragon/01 - Eragon - Christopher Paolini.epub" \
+  "organized/Christopher Paolini/Eragon/Christopher Paolini - Eragon.epub"
+cp "Christopher Paolini/mobi/01 - Eragon/01 - Eragon - Christopher Paolini.mobi" \
+  "organized/Christopher Paolini/Eragon/Christopher Paolini - Eragon.mobi"
+# Eldest
+mkdir -p "organized/Christopher Paolini/Eldest"
+cp "Christopher Paolini/epub/02 - Eldest/02 - Eldest - Christopher Paolini.epub" \
+  "organized/Christopher Paolini/Eldest/Christopher Paolini - Eldest.epub"
+cp "Christopher Paolini/mobi/02 - Eldest/02 - Eldest - Christopher Paolini.mobi" \
+  "organized/Christopher Paolini/Eldest/Christopher Paolini - Eldest.mobi"
+# Brisingr
+mkdir -p "organized/Christopher Paolini/Brisingr"
+cp "Christopher Paolini/epub/03 - Brisingr/03 - Brisingr - Christopher Paolini.epub" \
+  "organized/Christopher Paolini/Brisingr/Christopher Paolini - Brisingr.epub"
+cp "Christopher Paolini/mobi/03 - Brisingr/03 - Brisingr - Christopher Paolini.mobi" \
+  "organized/Christopher Paolini/Brisingr/Christopher Paolini - Brisingr.mobi"
+# Inheritance
+mkdir -p "organized/Christopher Paolini/Inheritance"
+cp "Christopher Paolini/epub/04 - Inheritance/04 - Inheritance - Christopher Paolini.epub" \
+  "organized/Christopher Paolini/Inheritance/Christopher Paolini - Inheritance.epub"
+cp "Christopher Paolini/mobi/04 - Inheritance/04 - Inheritance - Christopher Paolini.mobi" \
+  "organized/Christopher Paolini/Inheritance/Christopher Paolini - Inheritance.mobi"
+
+## Chuck Palahniuk
+mkdir -p "organized/Chuck Palahniuk"
+# Invisible Monsters
+mkdir -p "organized/Chuck Palahniuk/Invisible Monsters"
+cp "Chuck Palahniuk - Invisible Monsters (Remix)[1999,2012].epub" \
+  "organized/Chuck Palahniuk/Invisible Monsters/Chuck Palahniuk - Invisible Monsters.epub"
+
+## David Allen
+mkdir -p "organized/David Allen/"
+# Getting Things Done
+mkdir -p "organized/David Allen/Getting Things Done"
+cp "David Allen - Getting Things Done The Art of Stress-Free Productivity Revised 2015.azw" \
+  "organized/David Allen/Getting Things Done/David Allen - Getting Things Done.azw"
+
+## Miguel De Cervantes Saavedra
+mkdir -p "organized/Miguel De Cervantes Saavedra"
+# Don Quixote
+mkdir -p "organized/Miguel De Cervantes Saavedra/Don Quixote"
+cp "Don Quixote - Miguel De Cervantes Saavedra/Don Quixote - Miguel De Cervantes Saavedra.epub" \
+  "organized/Miguel De Cervantes Saavedra/Don Quixote/Miguel De Cervantes Saavedra - Don Quixote.epub"
+cp "Don Quixote - Miguel De Cervantes Saavedra/Don Quixote - Miguel De Cervantes Saavedra.mobi" \
+  "organized/Miguel De Cervantes Saavedra/Don Quixote/Miguel De Cervantes Saavedra - Don Quixote.mobi"
+
+## Daniel keyes
+mkdir -p "organized/Daniel Keyes"
+# Flowers for Algernon
+mkdir -p "organized/Daniel Keyes/Flowers For Algernon"
+cp "Flowers for Algernon - Daniel Keyes.epub" \
+  "organized/Daniel Keyes/Flowers For Algernon/Daniel Keyes - Flowers For Algernon.epub"
+
+## George Polya
+mkdir -p "organized/George Polya/How To Solve It"
+# How To Solve It
+cp "how_to_solve_it__polya_g.epub" \
+  "organized/George Polya/How To Solve It/George Polya - How To Solve It.epub"
+
+## Issac Marion
+# The New Hunger
+mkdir -p "organized/Isaac Marion/The New Hunger"
+cp "Isaac Marion - The New Hunger/The New Hunger - Isaac Marion.epub" \
+  "organized/Isaac Marion/The New Hunger/Isaac Marion - The New Hunger.epub"
+cp "Isaac Marion - The New Hunger/The New Hunger - Isaac Marion.mobi" \
+  "organized/Isaac Marion/The New Hunger/Isaac Marion - The New Hunger.mobi"
+# Warm Bodies
+mkdir -p "organized/Isaac Marion/Warm Bodies"
+cp "Isaac Marion - Warm Bodies/Warm Bodies_ A Novel - Isaac Marion.epub" \
+  "organized/Isaac Marion/Warm Bodies/Isaac Marion - Warm Bodies.epub"
+cp "Isaac Marion - Warm Bodies/Warm Bodies_ A Novel - Isaac Marion.mobi" \
+  "organized/Isaac Marion/Warm Bodies/Isaac Marion - Warm Bodies.mobi"
+
+## Susanna Clarke
+# Jonathan Strange And Mr Norrell
+mkdir -p "organized/Susanna Clarke/Jonathan Strange And Mr Norrell"
+cp "Jonathan_Strange_and_Mr_Norrell.epub" \
+  "organized/Susanna Clarke/Jonathan Strange And Mr Norrell/Susanna Clarke - Jonathan Strange And Mr Norrell.epub"
+
+## Joshua Ferris
+# Then We Came To The End
+mkdir -p "organized/Joshua Ferris/Then We Came To The End"
+cp "Joshua Ferris - Then We Came to the End/Then We Came to the End - Joshua Ferris.epub" \
+  "organized/Joshua Ferris/Then We Came To The End/Joshua Ferris - Then We Came To The End.epub" 
+cp "Joshua Ferris - Then We Came to the End/Then We Came to the End - Joshua Ferris.mobi" \
+  "organized/Joshua Ferris/Then We Came To The End/Joshua Ferris - Then We Came To The End.mobi" 
+
+## Kazuo Ishiguro
+# Never Let Me Go
+mkdir -p "organized/Kazuo Ishiguro/Never Let Me Go"
+cp "Kazuo Ishiguro - Never Let Me Go/Never Let Me Go - Kazuo Ishiguro.epub" \
+  "organized/Kazuo Ishiguro/Never Let Me Go/Kazuo Ishiguro - Never Let Me Go.epub"
+cp "Kazuo Ishiguro - Never Let Me Go/Never Let Me Go - Kazuo Ishiguro.mobi" \
+  "organized/Kazuo Ishiguro/Never Let Me Go/Kazuo Ishiguro - Never Let Me Go.mobi"
+
+## Lemony Snicket
+# A Series Of Unfortunate Events
+mkdir -p "organized/Lemony Snicket/The Bad Beginning/"
+cp "Lemony Snicket/A Series of Unfortunate Events 1_ The Bad Beginning (100)/A Series of Unfortunate Events 1_ The Bad Beginning - Lemony Snicket.epub" \
+  "organized/Lemony Snicket/The Bad Beginning/Lemony Snicket - The Bad Beginning.epub"
+cp "Lemony Snicket/A Series of Unfortunate Events 1_ The Bad Beginning (100)/A Series of Unfortunate Events 1_ The Bad Beginning - Lemony Snicket.mobi" \
+  "organized/Lemony Snicket/The Bad Beginning/Lemony Snicket - The Bad Beginning.mobi"
+mkdir -p "organized/Lemony Snicket/The Reptile Room"
+cp "Lemony Snicket/A Series of Unfortunate Events 2_ The Reptile Room (104)/A Series of Unfortunate Events 2_ The Reptile Room - Lemony Snicket.epub" \
+  "organized/Lemony Snicket/The Reptile Room/Lemony Snicket - The Reptile Room.epub"
+cp "Lemony Snicket/A Series of Unfortunate Events 2_ The Reptile Room (104)/A Series of Unfortunate Events 2_ The Reptile Room - Lemony Snicket.mobi" \
+  "organized/Lemony Snicket/The Reptile Room/Lemony Snicket - The Reptile Room.mobi"
+
+mkdir -p "organized/Lemony Snicket/The Wide Window Or Disappearance"
+cp "Lemony Snicket/A Series of Unfortunate Events 3_ The Wide Window_ Or, Disappearance! (93)/A Series of Unfortunate Events 3_ The Wide Window_ Or, Disappearance! - Lemony Snicket.epub" \
+  "organized/Lemony Snicket/The Wide Window Or Disappearance/Lemony Snicket - The Wide Window Or Disappearance!.epub"
+cp "Lemony Snicket/A Series of Unfortunate Events 3_ The Wide Window_ Or, Disappearance! (93)/A Series of Unfortunate Events 3_ The Wide Window_ Or, Disappearance! - Lemony Snicket.mobi" \
+  "organized/Lemony Snicket/The Wide Window Or Disappearance/Lemony Snicket - The Wide Window Or Disappearance!.mobi"
+mkdir -p "organized/Lemony Snicket/The Miserable Mill"
+cp "Lemony Snicket/A Series of Unfortunate Events 4_ The Miserable Mill (95)/A Series of Unfortunate Events 4_ The Miserable Mill - Lemony Snicket.epub" \
+  "organized/Lemony Snicket/The Miserable Mill/Lemony Snicket - The Miserable Mill.epub"
+cp "Lemony Snicket/A Series of Unfortunate Events 4_ The Miserable Mill (95)/A Series of Unfortunate Events 4_ The Miserable Mill - Lemony Snicket.mobi" \
+  "organized/Lemony Snicket/The Miserable Mill/Lemony Snicket - The Miserable Mill.mobi"
+mkdir -p "organized/Lemony Snicket/The Austere Academy"
+cp "Lemony Snicket/A Series of Unfortunate Events 5_ The Austere Academy (96)/A Series of Unfortunate Events 5_ The Austere Academy - Lemony Snicket.epub" \
+  "organized/Lemony Snicket/The Austere Academy/Lemony Snicket - The Austere Academy.epub"
+cp "Lemony Snicket/A Series of Unfortunate Events 5_ The Austere Academy (96)/A Series of Unfortunate Events 5_ The Austere Academy - Lemony Snicket.mobi" \
+  "organized/Lemony Snicket/The Austere Academy/Lemony Snicket - The Austere Academy.mobi"
+mkdir -p "organized/Lemony Snicket/The Ersatz Elevator"
+cp "Lemony Snicket/A Series of Unfortunate Events 6_ The Ersatz Elevator (94)/A Series of Unfortunate Events 6_ The Ersatz Elevator - Lemony Snicket.epub" \
+  "organized/Lemony Snicket/The Ersatz Elevator/Lemony Snicket - The Ersatz Elevator.epub"
+cp "Lemony Snicket/A Series of Unfortunate Events 6_ The Ersatz Elevator (94)/A Series of Unfortunate Events 6_ The Ersatz Elevator - Lemony Snicket.mobi" \
+  "organized/Lemony Snicket/The Ersatz Elevator/Lemony Snicket - The Ersatz Elevator.mobi"
+mkdir -p "organized/Lemony Snicket/The Vile Village"
+cp "Lemony Snicket/A Series of Unfortunate Events 7_ The Vile Village (99)/A Series of Unfortunate Events 7_ The Vile Village - Lemony Snicket.epub" \
+  "organized/Lemony Snicket/The Vile Village/Lemony Snicket - The Vile village.epub"
+cp "Lemony Snicket/A Series of Unfortunate Events 7_ The Vile Village (99)/A Series of Unfortunate Events 7_ The Vile Village - Lemony Snicket.mobi" \
+  "organized/Lemony Snicket/The Vile Village/Lemony Snicket - The Vile village.mobi"
+mkdir -p "organized/Lemony Snicket/The Hostile Hospital"
+cp "Lemony Snicket/A Series of Unfortunate Events 8_ The Hostile Hospital (105)/A Series of Unfortunate Events 8_ The Hostile Hospital - Lemony Snicket.epub" \
+  "organized/Lemony Snicket/The Hostile Hospital/Lemony Snicket - The Hostile Hospital.epub"
+cp "Lemony Snicket/A Series of Unfortunate Events 8_ The Hostile Hospital (105)/A Series of Unfortunate Events 8_ The Hostile Hospital - Lemony Snicket.mobi" \
+  "organized/Lemony Snicket/The Hostile Hospital/Lemony Snicket - The Hostile Hospital.mobi"
+mkdir -p "organized/Lemony Snicket/The Carnivorous Carnival"
+cp "Lemony Snicket/A Series of Unfortunate Events 9_ The Carnivorous Carnival (97)/A Series of Unfortunate Events 9_ The Carnivorous Carnival - Lemony Snicket.epub" \
+  "organized/Lemony Snicket/The Carnivorous Carnival/Lemony Snicket - The Carnivorous Carnival.epub"
+cp "Lemony Snicket/A Series of Unfortunate Events 9_ The Carnivorous Carnival (97)/A Series of Unfortunate Events 9_ The Carnivorous Carnival - Lemony Snicket.mobi" \
+  "organized/Lemony Snicket/The Carnivorous Carnival/Lemony Snicket - The Carnivorous Carnival.mobi"
+mkdir -p "organized/Lemony Snicket/The Slippery Slope"
+cp "Lemony Snicket/A Series of Unfortunate Events 10_ The Slippery Slope (102)/A Series of Unfortunate Events 10_ The Slippery Slope - Lemony Snicket.epub" \
+  "organized/Lemony Snicket/The Slippery Slope/Lemony Snicket - The Slippery Slope.epub"
+cp "Lemony Snicket/A Series of Unfortunate Events 10_ The Slippery Slope (102)/A Series of Unfortunate Events 10_ The Slippery Slope - Lemony Snicket.mobi" \
+  "organized/Lemony Snicket/The Slippery Slope/Lemony Snicket - The Slippery Slope.mobi"
+mkdir -p "organized/Lemony Snicket/The Grim Grotto"
+cp "Lemony Snicket/A Series of Unfortunate Events 11_ The Grim Grotto (98)/A Series of Unfortunate Events 11_ The Grim Grotto - Lemony Snicket.epub" \
+  "organized/Lemony Snicket/The Grim Grotto/Lemony Snicket - The Grim Grotto.epub"
+cp "Lemony Snicket/A Series of Unfortunate Events 11_ The Grim Grotto (98)/A Series of Unfortunate Events 11_ The Grim Grotto - Lemony Snicket.mobi" \
+  "organized/Lemony Snicket/The Grim Grotto/Lemony Snicket - The Grim Grotto.mobi"
+mkdir -p "organized/Lemony Snicket/The Penultimate Peril"
+cp "Lemony Snicket/A Series of Unfortunate Events 12_ The Penultimate Peril (103)/A Series of Unfortunate Events 12_ The Penultimate Peril - Lemony Snicket.epub" \
+  "organized/Lemony Snicket/The Penultimate Peril/Lemony Snicket - The Penultimate Peril.epub"
+cp "Lemony Snicket/A Series of Unfortunate Events 12_ The Penultimate Peril (103)/A Series of Unfortunate Events 12_ The Penultimate Peril - Lemony Snicket.mobi" \
+  "organized/Lemony Snicket/The Penultimate Peril/Lemony Snicket - The Penultimate Peril.mobi"
+mkdir -p "organized/Lemony Snicket/The End"
+cp "Lemony Snicket/A Series of Unfortunate Events 13_ The End (101)/A Series of Unfortunate Events 13_ The End - Lemony Snicket.epub" \
+  "organized/Lemony Snicket/The End/Lemony Snicket - The End.epub"
+cp "Lemony Snicket/A Series of Unfortunate Events 13_ The End (101)/A Series of Unfortunate Events 13_ The End - Lemony Snicket.mobi" \
+  "organized/Lemony Snicket/The End/Lemony Snicket - The End.mobi"
+
+## Neil Gaiman
+# American Gods
+mkdir -p "organized/Neil Gaiman/American Gods/"
+cp "MAM American Gods/American Gods (Tenth Anniversary Edition - Neil Gaiman.epub" \
+  "organized/Neil Gaiman/American Gods/Neil Gaiman - American Gods.epub"
+# Neverwhere
+mkdir -p "organized/Neil Gaiman/Neverwhere/"
+cp "Neverwhere - Neil Gaiman.epub" \
+  "organized/Neil Gaiman/Neverwhere/Neil Gaiman - Neverwhere.epub"
+
+## Ian McEwan
+# Atonement
+mkdir -p "organized/Ian McEwan/Atonement/"
+cp "McEwan,_Ian_-_Atonement..mobi/McEwan,_Ian_-_Atonement.mobi" \
+  "organized/Ian McEwan/Atonement/Ian McEwan - Atonement.mobi"
+
+## Paul Graham
+# Hackers And Painters
+mkdir -p "organized/Paul Graham/Hackers And Painters/"
+cp "Paul Graham - Hackers & Painters/Hackers & Painters - Paul Graham.epub" \
+  "organized/Paul Graham/Hackers And Painters/Paul Graham - Hackers And Painters.epub"
+cp "Paul Graham - Hackers & Painters/Hackers & Painters - Paul Graham.mobi" \
+  "organized/Paul Graham/Hackers And Painters/Paul Graham - Hackers And Painters.mobi"
+
+## Garth Nix
+# Sabriel
+mkdir -p "organized/Garth Nix/Sabriel/"
+cp "Sabriel - Garth Nix.mobi" \
+  "organized/Garth Nix/Sabriel/Garth Nix - Sabriel.mobi"
+
+## Melissa Broder
+# So Sad Today
+mkdir -p "organized/Melissa Broder/So Sad Today/"
+cp "so_sad_today_melissa_broder.epub" \
+  "organized/Melissa Broder/So Sad Today/Melissa Broder - So Sad Today.epub"
+
+## Junot Diaz
+# The Brief And Wonderous Life Of Oscar Wao
+mkdir -p "organized/Junot Diaz/The Brief And Wonderous Life Of Oscar Wao/"
+cp "The Brief Wondrous Life of Oscar Wao (20 (324)/The Brief Wondrous Life of Oscar Wao (20 - Junot Diaz.epub" \
+  "organized/Junot Diaz/The Brief And Wonderous Life Of Oscar Wao/Junot Diaz - The Brief And Wonderous Life Of Oscar Wao.epub"
+cp "The Brief Wondrous Life of Oscar Wao (20 (324)/The Brief Wondrous Life of Oscar Wao (20 - Junot Diaz.mobi" \
+  "organized/Junot Diaz/The Brief And Wonderous Life Of Oscar Wao/Junot Diaz - The Brief And Wonderous Life Of Oscar Wao.mobi"
+
+## Randall Monroe
+# What If
+mkdir -p "organized/Randall Monroe/What If/"
+cp "What If.epub" \
+  "organized/Randall Monroe/What If/Randall Monroe - What If.epub"
+
+## Douglas Hofstadter
+# I Am A Strange Loop
+mkdir -p "organized/Douglas Hofstadter/I Am A Strange Loop/"
+cp "Douglas Hofstadter - I Am a Strange Loop/I Am a Strange Loop - Douglas R. Hofstadter.mobi" \
+  "organized/Douglas Hofstadter/I Am A Strange Loop/Douglas Hofstadter - I Am A Strange Loop.mobi"
+cp "Douglas Hofstadter - I Am a Strange Loop/I Am a Strange Loop - Douglas R. Hofstadter.epub" \
+  "organized/Douglas Hofstadter/I Am A Strange Loop/Douglas Hofstadter - I Am A Strange Loop.epub"
+mkdir -p "organized/Douglas Hofstadter/Godel Escher Bach/"
+cp "Godel, Escher, Bach_Eternal Golden Braid - Hofstadter.mobi" \
+  "organized/Douglas Hofstadter/Godel Escher Bach/Douglas Hofstadter - Godel Escher Bach.mobi"
+
